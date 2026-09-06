aa738d64d70a24fb154326257a4a5fa3a9c18aa8
abenadero <abenadero.dev@protonmail.com>
2026-09-06 23:38:41 +0200

    Added some catppuccin colors

---
+++ colors/.config/colors/catppuccin-mocha.lua
@@ -0,0 +1,27 @@
+return {
+    text      = "rgb(cdd6f4)",
+    subtext1  = "rgb(bac2de)",
+    subtext0  = "rgb(a6adc8)",
+
+    overlay2  = "rgb(9399b2)",
+    overlay1  = "rgb(7f849c)",
+    overlay0  = "rgb(6c7086)",
+
+    surface2  = "rgb(585b70)",
+    surface1  = "rgb(45475a)",
+    surface0  = "rgb(313244)",
+
+    base      = "rgb(1e1e2e)",
+    mantle    = "rgb(181825)",
+    crust     = "rgb(11111b)",
+
+    mauve     = "rgb(cba6f7)",
+    lavender  = "rgb(b4befe)",
+    blue      = "rgb(89b4fa)",
+    teal      = "rgb(0000ff)",
+    green     = "rgb(a6e3a1)",
+    yellow    = "rgb(f9e2af)",
+    peach     = "rgb(fab387)",
+    red       = "rgb(f38ba8)",
+    rosewater = "rgb(f5e0dc)",
+}
