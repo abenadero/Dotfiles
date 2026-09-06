866dfe4307a526a84fe03249e6d2327bc4d8eb78
abenadero <abenadero.dev@protonmail.com>
2026-09-06 23:39:47 +0200

    Added waybar config

---
+++ waybar/.config/waybar/scripts/power-menu.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+selected=$(printf '󰐥  Apagar\n󰜉  Reiniciar\n󰍃  Cerrar sesión\n󰌾  Bloquear\n  Cerrar\n' |
+    wofi \
+        --dmenu \
+        --columns=2 \
+        --width=300 \
+        --height=154 \
+        --location=3 \
+        --hide-scroll \
+        --hide-search \
+        --xoffset -22 \
+        --yoffset 4 \
+        --cache-file=/dev/null \
+        --style="$HOME/.config/wofi/power-menu.css"
+)
+
+case "$selected" in
+    *Apagar*)
+        systemctl poweroff
+        ;;
+    *Reiniciar*)
+        systemctl reboot
+        ;;
+    *"Cerrar sesión"*)
+        hyprctl dispatch exit
+        ;;
+    *Bloquear*)
+        hyprlock
+        ;;
+esac
